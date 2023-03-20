9aa2b7ad5dcc13b4d7320c2a861e20200f085b55
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-20 21:56:41 +0100

    Normalización p3.sql 1.0

---
+++ Examenes/ex5/p3.sql
@@ -0,0 +1,11 @@
+-- Realiza el diseño físico de:
+-- Matricula (dni, nombres, apellidos, asignatura)
+-- Recuerda que tiene que estar normalizado
+
+-- 1FN
+CREATE TABLE Matricula (dni varchar(9) PRIMARY KEY NOT NULL,, nombres TEXT, apellidos TEXT, asignatura TEXT);
+
+-- 2FN
+CREATE TABLE Alumno (dni varchar(9) PRIMARY KEY NOT NULL, nombres TEXT, apellidos TEXT);
+
+CREATE TABLE Matricula (dni varchar(9) REFERENCES Alumno(dni), asignatura TEXT, PRIMARY KEY(dni,asignatura))
