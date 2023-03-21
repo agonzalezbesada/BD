3b49903ef2243f0e3b5ed6193029a1a28f3571f8
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-21 17:19:12 +0100

    Aclaración

--- Examenes/ex5/p4.sql
+++ Examenes/ex5/p4.sql
@@ -9,9 +9,9 @@ CREATE TABLE Recuerdos(idFoto INTEGER PRIMARY KEY, nombre TEXT, dni varchar(9), 
 
 -- 2FN - 3FN
 CREATE TABLE Recuerdos (idFoto INTEGER REFERENCES Foto(idFoto), idPersona REFERENCES Persona(idPersona), monumento TEXT REFERENCES Monumento(monumento));
-
+-- No borré el idPersona porque quizás por temas de privacidad no fuese deseabla acceder tan fácilmente al dni
 CREATE TABLE Foto (idFoto INTEGER PRIMARY KEY NOT NULL, tituloFoto TEXT, fecha timestamp, monumento TEXT);
 
-CREATE TABLE Monumento (nombreMonumento TEXT PRIMARY KEY NOT NULL, ciudad TEXT, antigüedadMonumento TEXT);
+CREATE TABLE Monumento (nombreMonumento TEXT PRIMARY KEY NOT NULL, ciudad TEXT, antiguedadMonumento TEXT);
 
 CREATE TABLE Persona (idPersona INTEGER PRIMARY KEY NOT NULL, dni varchar(9), nombre TEXT);
