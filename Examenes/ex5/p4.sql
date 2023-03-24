00b825a575d32aeaaa0435efed079c915117ef91
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2023-03-24 22:48:25 +0100

    p4.sql Corrección

--- Examenes/ex5/p4.sql
+++ Examenes/ex5/p4.sql
@@ -3,15 +3,12 @@
 -- Recuerda que tiene que estar normalizado
 -- Recuerdos(idFoto, idPersona, nombre, dni, títuloFoto, fecha,monumento, ciudad, antigüedadMonumento)
 
--- 1FN
-CREATE TABLE Recuerdos(idFoto INTEGER PRIMARY KEY, nombre TEXT, dni varchar(9), tituloFoto TEXT, fech timestamp, monumento TEXT, ciudad TEXT, antiguedadMonumento TEXT);
-
 
 -- 2FN - 3FN
-CREATE TABLE Recuerdos (idFoto INTEGER REFERENCES Foto(idFoto), idPersona REFERENCES Persona(idPersona), monumento TEXT REFERENCES Monumento(monumento));
+CREATE TABLE Recuerdos (idFoto INTEGER REFERENCES Foto(idFoto), idPersona REFERENCES Persona(idPersona), monumento varchar(80) REFERENCES Monumento(monumento));
 -- No borré el idPersona porque quizás por temas de privacidad no fuese deseabla acceder tan fácilmente al dni
-CREATE TABLE Foto (idFoto INTEGER PRIMARY KEY NOT NULL, tituloFoto TEXT, fecha timestamp, monumento TEXT);
+CREATE TABLE Foto (idFoto INTEGER PRIMARY KEY, tituloFoto varchar(20), fecha timestamp);
 
-CREATE TABLE Monumento (nombreMonumento TEXT PRIMARY KEY NOT NULL, ciudad TEXT, antiguedadMonumento TEXT);
+CREATE TABLE Monumento (monumento varchar(80) PRIMARY KEY, ciudad varchar(30), antiguedadMonumento varchar(10));
 
-CREATE TABLE Persona (idPersona INTEGER PRIMARY KEY NOT NULL, dni varchar(9), nombre TEXT);
+CREATE TABLE Persona (idPersona INTEGER PRIMARY KEY, dni varchar(9), nombre varchar(20));
